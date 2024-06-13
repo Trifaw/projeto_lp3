124b5eaa5f2102b84fa9e40b3dd0edb3ee412d3c
Giovanni Foliene Luciano <giovannibanibani@gmail.com>
2024-06-13 16:57:00 -0300

    juntando flask com as bibliotecas

--- projeto_lp3/projeto_lp3/app.py
+++ projeto_lp3/projeto_lp3/app.py
@@ -1,9 +1,15 @@
 from flask import Flask
+from validate_docbr import CPF, CNPJ
 
 app = Flask('Minha App')
 
 # rota + função
 # / - home page
+
+@app.route("/servicos")
+def servicos ():
+    return "<h1>Nossos Serviços<h1>"
+    
 
 @app.route('/')
 def home ():
@@ -19,3 +25,24 @@ def contato():
 @app.route('/produtos')
 def produtos():
     return '<h1>Produtos</h1>'
+
+cpf = CPF()
+
+@app.route("/gerar-cpf")
+def gerar_cpf():
+    
+    return f'CPF: {cpf.generate(True)}'
+
+cnpj = CNPJ()
+
+@app.route('/gerar-cnpj')
+def gerar_cnpj():
+
+    return f"CNPJ: {cnpj.generate(True)}"
+
+app.run()
+
+
+# página /serviços retornar "Nossos serviços"
+# página /gerar-cpf retornar retornar cpf aleatório
+# página /gerar-cnpj retornar cnpj aleatório
